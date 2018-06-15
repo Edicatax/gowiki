c8973d054574913ce04d2d8c6c484439a49f97e2
Yri Davies <0xdavy@gmail.com>
2018-06-15 08:21:57 +0200

    Rename renderPage to renderTemplate
    Reflects function better

--- wiki.go
+++ wiki.go
@@ -27,7 +27,7 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
-func renderPage(w http.ResponseWriter, tmpl string, p *Page) {
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
     t, _ := template.ParseFiles(tmpl + ".html")
     t.Execute(w, p)
 }
@@ -39,7 +39,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
         http.Redirect(w, r, "/edit/" + title, http.StatusFound)
         return
     }
-    renderPage(w, "view", p)
+    renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -48,7 +48,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         p = &Page{Title: title,}
     }
-    renderPage(w, "edit", p)
+    renderTemplate(w, "edit", p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
